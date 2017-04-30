3f1b3f41f118df5ef701bc41a318771409864b19
diana.raspopova <raspopova@improveitgroup.com>
2017-04-30 09:21:06 +0700

    details color scheme change

---
+++ XYZReader/src/main/java/com/example/xyzreader/utils/FabScrollBehavior.java
@@ -0,0 +1,50 @@
+package com.example.xyzreader.utils;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.example.xyzreader.R;
+
+/**
+ * Created by Diana.Raspopova on 4/29/2017.
+ */
+
+public class FabScrollBehavior  extends CoordinatorLayout.Behavior<FloatingActionButton> {
+    private int toolbarHeight;
+
+    public FabScrollBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.toolbarHeight = getToolbarHeight(context);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton fab, View dependency) {
+        return dependency instanceof AppBarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, FloatingActionButton fab, View dependency) {
+        if (dependency instanceof AppBarLayout) {
+            CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) fab.getLayoutParams();
+            int fabBottomMargin = lp.bottomMargin;
+            int distanceToScroll = fab.getHeight() + fabBottomMargin;
+            float ratio = (float)dependency.getY()/(float)toolbarHeight;
+            fab.setTranslationY(-distanceToScroll * ratio);
+        }
+        return true;
+    }
+
+    public static int getToolbarHeight(Context context) {
+        final TypedArray styledAttributes = context.getTheme().obtainStyledAttributes(
+                new int[]{R.attr.actionBarSize});
+        int toolbarHeight = (int) styledAttributes.getDimension(0, 0);
+        styledAttributes.recycle();
+
+        return toolbarHeight;
+    }
+}
